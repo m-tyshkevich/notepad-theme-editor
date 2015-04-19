2d89f1468af79c135d8cf9c13d318cd630c9e461
m-tyshkevich <margo1898@gmail.com>
2015-04-19 13:49:26 +0300

    Fixing some jQuery bugs

--- src/main/webapp/js/jquery-events.js
+++ src/main/webapp/js/jquery-events.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $(".button").mouseenter(function(){
     	$(this).css("color", "#ffffff");
     	$(this).css("border", "5px solid #ffffff");
+    	$(this).children("a").css("color", "#ffffff");
     });
 });
 
@@ -9,19 +10,6 @@ $(document).ready(function(){
     $(".button").mouseleave(function(){
     	$(this).css("color", "#919191");
     	$(this).css("border", "5px solid #919191");
-    });
-});
-
-$(document).ready(function(){
-    $("a").mouseenter(function(){
-    	$(this).css("color", "#ffffff");
-    	$(this).css("border", "5px solid #ffffff");
-    });
-});
-
-$(document).ready(function(){
-    $("a").mouseleave(function(){
-    	$(this).css("color", "#919191");
-    	$(this).css("border", "5px solid #919191");
+    	$(this).children("a").css("color", "#919191");
     });
 });
